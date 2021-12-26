a5265288849cf3c465ca2ce975ccee0defe626cb
Sanjeev Kumar <rajanala.sanjeev@gmail.com>
2021-12-26 13:27:27 -0500

    pushing the new code

--- src/main/java/Amazon.java
+++ src/main/java/Amazon.java
@@ -4,14 +4,14 @@ public class Amazon {
 
      Customer customer = new Customer();// JVM 10001 customer
 
-        customer.setName("Vinnie");
-        customer.setMobile("+1 248-938-3951");
+        customer.setName("XXXXX Mattam");
+        customer.setMobile("+1 248-XXX-XXXX");
         customer.setAddress("2344, Himaynagar, Hyderabad");
         customer.setPassword("vinni1234");
 
-//        System.out.println(customer.getName());
-//        System.out.println(customer.getMobile());
-//        System.out.println(customer.getAddress());
+        System.out.println(customer.getName());
+        System.out.println(customer.getMobile());
+        System.out.println(customer.getAddress());
 
 
       //  System.out.println(customer.vinnieOwnMethod());
